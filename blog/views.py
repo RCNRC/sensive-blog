7322c12d5ccf55bc8fdacc2959e9b2e512bf7a87
Ilya <Dragoilya@yandex.ru>
2023-03-13 02:26:03 +0300

    cleanup the code

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-from blog.models import Comment, Post, Tag
+from blog.models import Comment, Post, Tag, User
 from django.db.models import Count
 from django.db.models import Prefetch
-
-
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
 
 
 def serialize_post_optimized(post):
@@ -30,13 +16,6 @@ def serialize_post_optimized(post):
         'slug': post.slug,
         'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.first().title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
@@ -79,8 +58,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.annotate(likes_count=Count('likes')).get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    post = Post.objects.popular().get(slug=slug)
+    comments = post.comment_set.prefetch_related('author').all()
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -89,7 +68,7 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    related_tags = post.tags.popular().all()
+    related_tags = post.tags.popular()
 
     serialized_post = {
         'title': post.title,
@@ -104,14 +83,21 @@ def post_detail(request, slug):
     }
 
     most_popular_tags = Tag.objects.popular()[:5]
-    
-    most_popular_posts = Post.objects.popular() \
-                             .prefetch_related('author')[:5] \
-                             .fetch_with_comments_count()
+
+    most_popular_posts = Post.objects \
+        .prefetch_related('author', Prefetch(
+            'tags',
+            queryset=Tag.objects.popular()
+        )) \
+        .popular() \
+        .prefetch_related('author')[:5] \
+        .fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
-        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
+        'popular_tags': [
+            serialize_tag_optimized(tag) for tag in most_popular_tags
+        ],
         'most_popular_posts': [
             serialize_post_optimized(post) for post in most_popular_posts
         ],
@@ -124,15 +110,24 @@ def tag_filter(request, tag_title):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = Post.objects.popular() \
-                             .prefetch_related('author')[:5] \
-                             .fetch_with_comments_count()
+    most_popular_posts = Post.objects \
+        .prefetch_related('author', Prefetch(
+            'tags',
+            queryset=Tag.objects.popular()
+        )) \
+        .popular() \
+        .prefetch_related('author')[:5] \
+        .fetch_with_comments_count()
 
-    related_posts = tag.posts.annotate(comments_count=Count('comment'))[:20]
+    related_posts = tag.posts.prefetch_related('author', Prefetch(
+            'tags', queryset=Tag.objects.popular()
+        )).annotate(comments_count=Count('comment'))[:20]
 
     context = {
         'tag': tag.title,
-        'popular_tags': [serialize_tag_optimized(tag) for tag in most_popular_tags],
+        'popular_tags': [
+            serialize_tag_optimized(tag) for tag in most_popular_tags
+        ],
         'posts': [serialize_post_optimized(post) for post in related_posts],
         'most_popular_posts': [
             serialize_post_optimized(post) for post in most_popular_posts
